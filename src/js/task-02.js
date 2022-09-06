a105c0345d3a2e2322df1ac49fce58aa03be9899
IreneCreadora <nargiza.dancer@gmail.com>
2022-09-06 16:59:57 +0300

    createSecondJS

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,22 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+// Напиши скрипт, який для кожного елемента масиву ingredients:
+// Створить окремий елемент <li>. Обов'язково використовуй метод document.createElement().
+// Додасть назву інгредієнта як його текстовий вміст.
+// Додасть елементу клас item.
+// Після чого, вставить усі <li> за одну операцію у список ul#ingredients.
+const listIngredientsRef = document.querySelector('ul#ingredients');
+
+const makeListIngredients = ingredients => {
+  return ingredients.map(ingredient => {
+    const itemOfIngredients = document.createElement('li');
+    itemOfIngredients.classList.add('item');
+    itemOfIngredients.textContent = ingredient;
+
+    return itemOfIngredients;
+  });
+};
+
+const elements = makeListIngredients(ingredients);
+listIngredientsRef.append(...elements);
