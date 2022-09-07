0bad9f8c94e7d049226deedc7b312b05fe140119
IreneCreadora <nargiza.dancer@gmail.com>
2022-09-07 12:22:41 +0300

    createThirdJS

--- src/js/task-03.js
+++ src/js/task-03.js
@@ -12,3 +12,18 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+// Напиши скрипт для створення галереї зображень на підставі масиву даних. HTML містить список ul.gallery.
+// Використовуй масив об'єктів images для створення елементів <img>, вкладених в <li>.
+//  Для створення розмітки використовуй шаблонні рядки і метод insertAdjacentHTML().
+// Усі елементи галереї повинні додаватися в DOM за одну операцію додавання.
+// Додай мінімальне оформлення галереї флексбоксами або грідами через CSS класи.
+
+const imagesListRef = document.querySelector('ul.gallery');
+
+const makeGalleryRef = images
+  .map(
+    ({ url, alt }) =>
+      `<li class="gallery__item"><a href = #><img src=${url} alt=${alt} class="gallery__img" width="720"></a></li>`
+  )
+  .join('');
+imagesListRef.insertAdjacentHTML('beforeend', makeGalleryRef);
