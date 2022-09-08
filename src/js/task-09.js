ec9cdc2600b6b4b6238b770d84bea52584fc0aee
IreneCreadora <nargiza.dancer@gmail.com>
2022-09-08 16:03:12 +0300

    createNinthJS

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,19 @@
+// Напиши скрипт, який змінює кольори фону елемента < body > через інлайн - стиль
+// по кліку на button.change - color і виводить значення кольору в span.color.
+const refs = {
+  body: document.querySelector('body'),
+  colorName: document.querySelector('.color'),
+  btnRandomColor: document.querySelector('.change-color'),
+};
+
+refs.btnRandomColor.addEventListener('click', changeBodyColor);
+
+function changeBodyColor(event) {
+  const color = getRandomHexColor();
+  refs.body.style.backgroundColor = color;
+  refs.colorName.textContent = color;
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
