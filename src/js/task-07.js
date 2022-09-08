c4f42836fc308622bc2554bab43148c00c97bf5d
IreneCreadora <nargiza.dancer@gmail.com>
2022-09-08 13:05:42 +0300

    createSeventhJS

---
+++ src/js/task-07.js
@@ -0,0 +1,11 @@
+// Напиши скрипт, який реагує на зміну значення input#font-size-control(подія input)
+//  і змінює інлайн - стиль span#text, оновлюючи властивість font-size.В результаті,
+//     перетягуючи повзунок, буде змінюватися розмір тексту.
+const rangeEl = document.querySelector('#font-size-control');
+const textEl = document.querySelector('#text');
+
+rangeEl.addEventListener('input', onInputChange);
+
+function onInputChange(event) {
+  text.style.fontSize = `${event.currentTarget.value}px`;
+}
