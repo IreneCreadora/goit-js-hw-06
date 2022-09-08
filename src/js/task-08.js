4c30b0a4f017249d75165c46d0b8d3585a691664
IreneCreadora <nargiza.dancer@gmail.com>
2022-09-08 15:43:35 +0300

    createEighthJS

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,32 @@
+// Обробка відправлення форми form.login-form повинна відбуватися відповідно до події submit.
+// Під час відправлення форми сторінка не повинна перезавантажуватися.
+// Якщо у формі є незаповнені поля, виводь alert з попередженням про те,
+//  що всі поля повинні бути заповнені.
+// Якщо користувач заповнив усі поля і відправив форму,
+// збери значення полів в об'єкт, де ім'я поля буде ім'ям властивості,
+// значення поля - значенням властивості.
+//  Для доступу до елементів форми використовуй властивість elements.
+// Виведи об'єкт із введеними даними в консоль і очисти значення полів форми методом reset.
 
+const formRef = document.querySelector('.login-form');
+
+const handleSubmit = event => {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === '' || password.value === '') {
+    alert('Заповніть всі поля!');
+    return;
+  }
+
+  const userData = {
+    email: email.value,
+    password: password.value,
+  };
+  console.log(userData);
+  event.currentTarget.reset();
+};
+formRef.addEventListener('submit', handleSubmit);
